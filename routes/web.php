d5f75dd11e51ea8100019a6545a24da348e86a77
= <=>
2022-01-11 01:00:42 +0100

    i creat 404 page not found

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ route::get('posts/{post}', function ($slug) {
 
     $path = __DIR__ . "/../resources/posts/{$slug}.html";
     if (! file_exists($path)){
-        dd('file does not exist');
+        abort(404);
     }
     $post =file_get_contents($path);
     return view('post', [
