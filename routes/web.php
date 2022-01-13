b2ed96499a6b2467d35def6c2bc5dd55315cd245
= <=>
2022-01-14 00:13:28 +0100

    i use the cache methode

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,22 @@ route::get('posts/{post}', function ($slug) {
 
     $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    ddd($path);
 
     if (! file_exists($path)){
-        return redirect('posts');
+        return redirect('/');
     }
-    $post =file_get_contents($path);
+
+    $post = cache()->remember("posts.{slug}", 5, function () use ($path) {
+
+        var_dump('file_get_contents');
+
+        return file_get_contents($path);
+    });
+
+    // $post =file_get_contents($path);
+
+
+
     return view('post', [
         'post' => $post
     ]);
